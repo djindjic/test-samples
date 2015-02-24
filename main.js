68e4657d505cc5f33bef0040b634c6af2cc768d7
Aleksandar Djindjic <djindjic@gmail.com>
2015-02-24 13:36:36 +0100

    famous sample

--- main.js
+++ main.js
@@ -1,6 +1,41 @@
 import angular from 'angular';
-import {myControllerModule} from 'myController.module';
+import 'famous-angular';
 
-let myApp = angular.module('myApp', [
-  myControllerModule.name
+angular.element(document).ready(function() {
+	angular.bootstrap(document.querySelector('[data-main-app]'), [
+		mainModule.name
+	], {
+		strictDi: true
+	});
+});
+
+export let mainModule = angular.module('famous-sample', [
+	'famous.angular'
 ]);
+
+mainModule.controller('MainCtrl', ['$scope', '$famous', '$timeout', function($scope, $famous, $timeout){
+	var Transitionable = $famous['famous/transitions/Transitionable'];
+	var Easing = $famous['famous/transitions/Easing'];
+	var t = new Transitionable(0);
+
+
+	$scope.myTransitionable = new Transitionable([0, 0, 0]);
+	//$scope.myTransitionable.set([100, 0, 0], {duration: 1000, curve: 'easeInOut'})
+
+	function reset(){
+		t.set(0);
+		t.set(Math.PI * 2.0, {duration: 3000, curve: 'linear'}, function(){
+			//$timeout(reset);
+		});
+		$scope.myTransitionable.set([300, 0, 0], {duration: 3000, curve: Easing.outBounce}, function(){
+			//$timeout(reset);
+		});
+	}
+
+	reset();
+
+	$scope.getRotateY = function(){
+		return t.get();
+	}
+	$scope.greetMe = 'world';
+}]);
--- myController.module.js
+++
@@ -1,7 +0,0 @@
-import angular from 'angular';
-
-export let myControllerModule = angular.module('myControllerModule', []);
-
-myControllerModule.controller('myController', [function() {
-  this.name = 'test controller';
-}]);
